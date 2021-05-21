2553d8eeb5de674d70522c47c0747aeaecd4a198
huangzihao.01 <huangzihao.01@bytedance.com>
2021-05-21 11:54:46 +0800

    每次游戏结果写入guess.csv中

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import random
+import csv
+import datetime
 from hangmanlib import print_hangman
 
+start_time = datetime.datetime.now()
+time_span = 0
+user_input = ''
 mistakes = 0
 real_word = ''
 guess_word = ''
 
 
 def init():
-    global real_word, guess_word, mistakes
+    global real_word, guess_word, mistakes, start_time, user_input
     print('Welcome to the Hangman game!')
+    start_time = datetime.datetime.now()
+    user_input = ''
     mistakes = 0
     with open("words.txt", "r") as file:
         data = file.read().split()
@@ -24,10 +31,12 @@ def print_stage():
 
 
 def handle_input():
-    letter = input('press a-z to guess')
+    global user_input
+    letter = input('press a-z to guess: ')
     if len(letter) != 1 or not 'a' <= letter <= 'z':
         print('just press one letter from a to z, try again')
         letter = handle_input()
+    user_input += letter
     return letter
 
 
@@ -42,23 +51,38 @@ def calculate(letter):
 
 
 def continue_or_quit():
-    decision = input('press c to play again and q to quit')
+    global start_time, time_span
+    time_span = (datetime.datetime.now() - start_time).total_seconds()
+    save_result()
+    decision = input('press c to play again and q to quit: ')
     while (len(decision) != 1 or (decision != 'c' and decision != 'q')):
-        decision = input('just press one letter:c or q')
+        decision = input('just press one letter:c or q: ')
     if decision == 'c':
         start_one_game()
     else:
-        SystemExit(0)
+        # SystemExit(0)
+        import sys
+        sys.exit(0)
+
+
+def save_result():
+    with open(r'./guess.csv', mode='a', newline='', encoding='utf8') as cfa:
+        wf = csv.writer(cfa)
+        # 写入游戏开始的时间,单次游戏使用的时间,猜测的单词,用户猜测的字符序列
+        data2 = [[start_time, str(time_span)+'秒', real_word, user_input]]
+        for i in data2:
+            wf.writerow(i)
 
 
 def check_result():
     if mistakes == 6:
         print_stage()
+        print('The word is %s', real_word)
         print('Sorry, you failed the game!')
         continue_or_quit()
     elif '_' not in guess_word:
-        print('Congratulations! You won the game.')
         print('The word is %s', real_word)
+        print('Congratulations! You won the game.')
         continue_or_quit()
 
 
